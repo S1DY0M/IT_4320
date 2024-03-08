0cab33355b5be429042256cdb6e9fe0f900beeeb
Austin Barr <97968882+S1DY0M@users.noreply.github.com>
2024-03-08 13:00:13 -0600

    Efficient version of FizzBuzz

---
+++ FizzBuzz02.py
@@ -0,0 +1,9 @@
+# Efficient Version of FizzBuzz
+
+for num in range(1, 101):
+    output = ""
+    if num % 3 == 0:
+        output += "Fizz"
+    if num % 5 == 0:
+        output += "Buzz"
+    print(output or num)
